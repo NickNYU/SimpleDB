9075fca776a7fec2e4c6995fc9ceaf2dae746922
NickNYU <cz739@nyu.edu>
2023-11-04 19:22:57 +0800

    [optimize]reduce non-impact classes and rename class for better understanding

--- src/main/java/simpledb/execution/aggregator/AggregatedGroup.java
+++ src/main/java/simpledb/execution/aggregator/AggregatedGroup.java
@@ -1,16 +1,14 @@
 package simpledb.execution.aggregator;
 
-import simpledb.storage.Field;
 import simpledb.storage.IntField;
 import simpledb.storage.Tuple;
-import simpledb.storage.TupleDesc;
 
 /**
  * @author nick
  * @e-mail cz739@nyu.edu
  * 2023/11/4
  */
-public interface GroupAggregator {
+public interface AggregatedGroup {
 
     void aggregate(Tuple tuple);
 
